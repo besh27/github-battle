4cf19d2aa9dc1278fe45613fa88f59af4603cd21
Esh, Benjamin <Benjamin.Esh@united.com>
2020-02-20 08:59:18 -0600

    refactor components

--- src/Components/Mode.js
+++ src/Components/Mode.js
@@ -1,44 +1,48 @@
+import React from 'react';
+
 class Mode extends React.Component {
-    constructor(props) {
-      super(props)
-      
-      this.state = {
-        mode: 'light' 
-      }
-      this.handleMode = this.handleMode.bind(this)
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mode: 'light'
     }
-    handleLightMode() {
-      // Change 'mode' on the component's state to 'light'
+    this.handleMode = this.handleMode.bind(this)
+  }
+  handleLightMode() {
+    // Change 'mode' on the component's state to 'light'
+    this.setState({
+      mode: 'light'
+    })
+  }
+  handleMode() {
+    this.state.mode !== 'light' ?
       this.setState({
         mode: 'light'
+      }) :
+      this.setState({
+        mode: 'dark'
       })
-    }
-    handleMode() {
-      this.state.mode !== 'light' ?
-        this.setState({
-          mode: 'light'
-        }) :
-        this.setState({
-          mode: 'dark'
-        })
-    }
-    render() {
-      const { mode } = this.state
-      
-      return (
-        <div style={{
-          height: '100%',
-          background: mode === 'light' ? '#fff' : '#000'
-        }}>
-          {mode === 'light'
-            ? <button onClick={this.handleMode}>Dark Mode</button>
-            : <button onClick={this.handleMode}>Light Mode</button>}
-        </div>
-      )
-    }
   }
-  
-  ReactDOM.render(
-    <Container />,
-    document.getElementById('app')
-  );
+  render() {
+    const { mode } = this.state
+
+    return (
+      <div style={{
+        height: '100%',
+        background: mode === 'light' ? '#fff' : '#000'
+      }}>
+        {mode === 'light'
+          ? <button onClick={this.handleMode}>Dark Mode</button>
+          : <button onClick={this.handleMode}>Light Mode</button>}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Container />,
+  document.getElementById('app')
+);
+
+export default Mode;

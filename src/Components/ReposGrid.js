411dbded0e88d9cb36a9fe39d4dff28994bb2615
besh27 <beshdevelopment@gmail.com>
2020-02-24 07:40:17 -0600

    Card Component refactor with children

--- src/Components/ReposGrid.js
+++ src/Components/ReposGrid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle, FaCode } from 'react-icons/fa';
-
+import Card from './Card';
 function ReposGrid({ repos }) {
     return (
         <ul className="grid space-around">
@@ -11,39 +11,33 @@ function ReposGrid({ repos }) {
 
                 return (
                     <li key={html_url} className="card bg-light">
-                        <h4 className="header-lg center-text">
-                            #{index + 1}
-                        </h4>
-                        <img
-                            className="avatar"
-                            src={avatar_url}
-                            alt={`Avatar for ${login}`}
-                        />
-                        <h2 className='center-text'>
-                            <a className="link" href={html_url}>
-                                {login}
-                            </a>
-                        </h2>
-                        <ul className='card-list'>
-                            <li>
-                                <FaUser color="rgb(255, 191, 116)" size={22} />
-                                <a href={`https://github.com/${login}`}>
-                                    {login}
-                                </a>
+                        <Card
+                            header={`#${index + 1}`}
+                            avatar={avatar_url}
+                            href={html_url}
+                            name={login}
+                        >
+                            <ul className='card-list'>
+                                <li>
+                                    <FaUser color="rgb(255, 191, 116)" size={22} />
+                                    <a href={`https://github.com/${login}`}>
+                                        {login}
+                                    </a>
+                                </li>
+                                <li>
+                                    <FaStar color="rgb(255, 215, 0)" size={22} />
+                                    {stargazers_count.toLocaleString()} stars
                             </li>
-                            <li>
-                                <FaStar color="rgb(255, 215, 0)" size={22} />
-                                {stargazers_count.toLocaleString()} stars
+                                <li>
+                                    <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                                    {forks.toLocaleString()} forks
                             </li>
-                            <li>
-                                <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
-                                {forks.toLocaleString()} forks
+                                <li>
+                                    <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                                    {open_issues.toLocaleString()} open issues
                             </li>
-                            <li>
-                                <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                                {open_issues.toLocaleString()} open issues
-                            </li>
-                        </ul>
+                            </ul>
+                        </Card>
                     </li>
                 )
             })}
